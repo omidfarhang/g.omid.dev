d51ec34cb2c686390aa6f851697bfa21b8d8be09
Omid Farhang <boelectronic@gmail.com>
2022-11-23 14:43:19 +0330

    minor change in HTML

--- src/views/invalid.ts
+++ src/views/invalid.ts
@@ -2,18 +2,17 @@ export const tmpl404 = `
 <!DOCTYPE html>
 <html>
 <head>
-<title>Error • eartharoid:go</title>
+<title>Error • g.omid.dev</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulmaswatch/0.8.1/darkly/bulmaswatch.min.css">
 <link rel="icon" href="/favicon.ico" type="image/icon">
-<link rel="icon" href="https://eartharoid.me/favicon.ico" type="image/icon">
+<link rel="icon" href="https://omid.dev/assets/logo/favicon.ico" type="image/icon">
 </head>
 <body>
 <section class="section">
 <container class="container box has-text-centered" style="max-width:500px;">
-<img class="block" src="https://img.eartharoid.me/insecure/plain/s3://eartharoid/eartharoid/logo/c/wordmark-with-blur.png@webp" style="max-width:300px;margin-top:1em;">
 <div class="content">
 <h6 class="is-uppercase">Error</h6>
 <p>
--- src/views/preview.ts
+++ src/views/preview.ts
@@ -1,28 +1,27 @@
 export const tmplPreview = `<!DOCTYPE html>
 <html>
 <head>
-<title>Link Preview • eartharoid:go</title>
+<title>Link Preview • g.omid.dev</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="theme-color" content="#00FFFF">
 <meta name="title" content="Link Preview">
 <meta name="og:title" content="Link Preview" />
-<meta name="og:site_name" content="eartharoid:go" />
+<meta name="og:site_name" content="g.omid.dev" />
 <meta name="description" content="Link preview for {{id}}">
 <meta name="og:description" content="Link preview for {{id}}" />
 <meta name="url" content="/{{id}}~">
 <meta name="og:url" content="/{{id}}~" />
-<meta name="og:image" content="https://img.eartharoid.me/insecure/plain/s3://eartharoid/eartharoid/logo/c/icon.png@webp" />
+<meta name="og:image" content="https://omid.dev/assets/images/2019/01/16583454_210922376050059_7816295726543011840_n-1-300x300.jpg" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulmaswatch/0.8.1/darkly/bulmaswatch.min.css">
 <link rel="icon" href="/favicon.ico" type="image/icon">
-<link rel="icon" href="https://eartharoid.me/favicon.ico" type="image/icon">
+<link rel="icon" href="https://omid.dev/assets/logo/favicon.ico" type="image/icon">
 <script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"></script>
 </head>
 <body>
 <section class="section">
 <container class="container box has-text-centered" style="max-width:500px;">
-<img class="block" src="https://img.eartharoid.me/insecure/plain/s3://eartharoid/eartharoid/logo/c/wordmark-with-blur.png@webp" style="max-width:300px;margin-top:1em;">
 <div class="content">
 <h6 class="is-uppercase">Link Preview</h6>
 <img src="/{{id}}.png" width="150px" height="auto" style="padding:1em;" />
