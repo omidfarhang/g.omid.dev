a7a4269a9a8cf6c2b98bba0e1ac13608205da832
Omid Farhang <boelectronic@gmail.com>
2022-11-27 18:47:34 +0330

    revert routres

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,7 @@ import QrHandler from './handlers/qrcode';
 import ShortlinkHandler from './handlers/shortlink';
 import ApiHandler from './handlers/api';
 
-
 const router = Router();
-
 
 router.get('/favicon.ico', () => Response.redirect('https://omid.dev/favicon.ico', 301))
 	.get('/yourls-api.php', ApiHandler)
@@ -19,16 +17,6 @@ router.get('/favicon.ico', () => Response.redirect('https://omid.dev/favicon.ico
 	.get('/:id', ShortlinkHandler)
 	.get('/', () => Response.redirect('https://omid.dev', 302));
 
-export default {
-	async fetch(request: Request) {
-					try {
-									return handleRequest(request);
-					} catch (e) {
-									return new Response(`${e}`);
-					}
-	},
-};
-
-async function handleRequest(request: Request) {
-	return router.handle(request)
-}
+addEventListener('fetch', event => {
+	event.respondWith(router.handle(event.request));
+});
